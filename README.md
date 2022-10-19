49c3c2b382b6c39c589b296060f95e67d78c6f52
Amir-17 <amirkahriman17@gmail.com>
2022-10-19 11:35:24 +0200

    my first game

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # number-guessing-game
-programming my first game using react, css, html
+
+programming my first game using react, css
+
+first of all declared all variables for this project,
+
+const [numberToGuess, setNumberToGuess] = useState(); - this is for
+
+const [userInput, setUserInput] = useState(0); - this is for for user input sector
+
+const [previousAttempts, setPreviousAttempts] = useState([]); - i used this to show useres previous attempts in game
+
+const [isWinner, setIsWinner] = useState(false); - this one is used for announcing the winner
+
+const [numberOfAttempts, setNumberOfAttempts] = useState(10); - this is for remaining attempts of player and used for function
+
+const [alertMessage, setAlertMessage] = useState(); - displaying all messages for user
+
+const [isDisabled, setIsDisabled] = useState(false); - for button disabling condition
+
+couple of function like gameOver, gueesNumber and resetGame
+
+The game is very simple, you need to guess random generated number, all button will be disabled on the beggining until you fill the number, after your submit of number alert message will pop out and tell you if your attempt was higher or lower number, you have 10 attempts, you have 2 more button for reset - that reset the game to the begining and clear - that clears your input field, when attempts counter comes to 0 game is over and you can play it again, if you submit correct answer pop out message with animation of victory will come to your display and yet againg you can start a new game.
---
+++ number-guessing-game/src/App.js
@@ -0,0 +1,126 @@
+import './App.css';
+import { useState, useEffect } from "react";
+
+function App() {
+  const [numberToGuess, setNumberToGuess] = useState();
+  const [userInput, setUserInput] = useState(0);
+  const [previousAttempts, setPreviousAttempts] = useState([]);
+  const [isWinner, setIsWinner] = useState(false);
+  const [numberOfAttempts, setNumberOfAttempts] = useState(10);
+  const [alertMessage, setAlertMessage] = useState();
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    setNumberToGuess(Math.round(Math.random() * 100) + 1)
+  }, []);
+
+
+  const gameOver = (guesses, guess) => {
+    if (guesses - 1 === 0) {
+      setAlertMessage(<p className='bg-warning'>GAME OVER!</p>);
+      setPreviousAttempts([...previousAttempts, guess]);
+      setIsDisabled(true);
+    }
+  };
+
+  const guessNumber = () => {
+    var userGuess = parseInt(userInput);
+
+    if (numberOfAttempts > 0) {
+      if (userGuess < 1 || userGuess > 100) {
+        setAlertMessage(<p className='aler-range'>Please enter a number in range 1 to 100</p>);
+      } else if (previousAttempts.includes(userGuess)) {
+        setAlertMessage(<p className='alert-info'>You have already entered this number!</p>);
+      } else if (userGuess === numberToGuess) {
+        setAlertMessage(<p className='bg-success'>Congratulations! You got it right!</p>);
+        setIsWinner(true);
+        setIsDisabled(true);
+      } else if (userGuess > numberToGuess) {
+        setAlertMessage(<p className='bg-danger'>UPS! The last guess was too high!</p>);
+        setPreviousAttempts([...previousAttempts, userGuess]);
+        setNumberOfAttempts(numberOfAttempts - 1);
+        gameOver(numberOfAttempts, userGuess);
+      } else if (userGuess < numberToGuess) {
+        setAlertMessage(<p className='bg-info'>UPS! The last guess was too low!</p>);
+        setPreviousAttempts([...previousAttempts, userGuess]);
+        setNumberOfAttempts(numberOfAttempts - 1);
+        gameOver(numberOfAttempts, userGuess);
+      }
+    }
+  };
+
+  const resetGame = () => {
+    setNumberToGuess(Math.round(Math.random() * 100) + 1);
+    setUserInput(0);
+    setPreviousAttempts([]);
+    setIsWinner(false);
+    setNumberOfAttempts(10);
+    setAlertMessage();
+    setIsDisabled(false);
+  };
+
+  return (
+    <div className='App'>
+      <h1>Number Guessing Game</h1>
+      <h3>Guess a number between 1 and 100</h3>
+
+      <div>
+        <>
+          <label>Enter a number :</label>
+          
+          <input
+            className='input'
+            type="number"
+            placeholder='Enter a number'
+            value={userInput}
+            onChange={(e) => {
+              e.preventDefault();
+              setUserInput(e.target.value);
+            }}
+          />
+        </>
+
+        <div>
+          <button
+            className='button'
+            onClick={() => guessNumber()}
+            disabled={userInput === 0 || userInput === null || isDisabled} >
+            Submit Number
+          </button>
+
+          <button
+            className='button'
+            disabled={isDisabled}
+            onClick={() => setUserInput(0)}>
+            Clear
+          </button>
+
+          <button
+            className='button'
+            disabled={isDisabled}
+            onClick={() => resetGame()}>
+            Reset
+          </button>
+        </div>
+        <p>Remaining attempts: {numberOfAttempts}</p>
+        {previousAttempts.length > 0 && (
+          <p>
+            Previous guesses: {" "}
+            {previousAttempts.map((attempt, i) => (
+              <span key={i}>
+                {attempt}
+                {previousAttempts.length - 1 !== i && ", "}
+              </span>
+            ))}
+          </p>
+        )}
+        {alertMessage}
+        {(isWinner || numberOfAttempts === 0) && (
+          <button className='start' onClick={() => resetGame()}>Start New Game</button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
